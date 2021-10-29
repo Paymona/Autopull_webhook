e51587588f23abfdc73d035cd8d47718d8b2bc7e
root <root@vm2814774.62ssd.had.wf>
2021-10-29 12:43:15 +0000

    adf

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from config import username, password
 from git import Repo
 from models import Repository
+import nginx
 app = Flask(__name__)
 
 
@@ -35,6 +36,33 @@ def register():
         "code" : repo.code,
     })
 
-@app.route('/register_domain')
+@app.route('/register_react')
 def register_domain():
     _domain = request.json.get('domain', -1)
+    _code = request.json.get('code', -1)
+    if _code == -1 or _domain == -1:
+        return jsonify(msg="Missing Something"), 400
+    
+    try:
+        repo_query = Repo.get(Repo.code == _code)
+    except:
+        return jsonify(msg="Project Not Found")
+
+    c = nginx.Conf()
+    s = nginx.Server()
+    s.add(
+            nginx.Key('listen', '80')
+            nginx.Comment("Automated nginx conf from webhook")
+            nginx.Key('server_name', f"{_domain} www.{_domain}")
+            nginx.Key('root', repo_query.path)
+            nginx.Key('index', "index.html index.htm index.nginx-debian.html")
+            nginx.Location("/",
+                nginx.Key("try_files", "$uri $uri/ =404")
+            )
+    )
+    c.add(s)
+    nginx.dumpf(c, f'/etc/nginx/sites-available/{_domain}')
+
+
+if __name__ == '__main__':
+    app.run(port=5001)
